3dd8c4dd6064faacf434177e4219484b5ac5ed49
Evgeny Glukhov <evglukhov@luxoft.com>
2018-05-11 10:50:14 +0300

    fix tests and change title

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('native-timer-app App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should contain root tag', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getRoot()).toBeDefined();
   });
 });
