509dff713bf5f3f645bd8f29beea7fad918be46d
Evgeny Glukhov <evglukhov@luxoft.com>
2018-05-13 23:22:18 +0300

    add responsiveness

--- src/app/modules/canvas/canvas.module.ts
+++ src/app/modules/canvas/canvas.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CanvasComponent } from './components/canvas/canvas.component';
+import { GeneralModule } from '../../general/general.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    GeneralModule
   ],
   declarations: [
     CanvasComponent
