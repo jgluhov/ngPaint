1d64228c9109fa37c936b4d9e2a72df1f1248dac
Evgeny Glukhov <evglukhov@luxoft.com>
2018-05-14 08:10:40 +0300

    add responsive styles

--- src/app/components/options-bar/options-bar.component.ts
+++ src/app/components/options-bar/options-bar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./options-bar.component.scss']
 })
 export class OptionsBarComponent implements OnInit {
-
+  title = 'Adjustments';
   constructor() { }
 
   ngOnInit(): void {
