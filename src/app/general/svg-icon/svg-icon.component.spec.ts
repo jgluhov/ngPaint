7c2abde0bb26026de8a4109c9dbabce1372674a1
Evgeny Glukhov <evglukhov@luxoft.com>
2018-05-20 08:14:43 +0300

    add tests for svg icon component

--- src/app/general/svg-icon/svg-icon.component.spec.ts
+++ src/app/general/svg-icon/svg-icon.component.spec.ts
@@ -1,11 +1,14 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MockBackend } from '@angular/http/testing';
-import { Http, BaseRequestOptions } from '@angular/http';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { Http, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
 import { SvgIconComponent } from './svg-icon.component';
 
 describe('SvgIconComponent', () => {
   let component: SvgIconComponent;
   let fixture: ComponentFixture<SvgIconComponent>;
+  let backend: MockBackend;
+  let nativeElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,15 +28,34 @@ describe('SvgIconComponent', () => {
       ]
     })
     .compileComponents();
+
+    backend = TestBed.get(MockBackend);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SvgIconComponent);
     component = fixture.componentInstance;
+    nativeElement = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#load()', () => {
+    beforeEach(() => {
+      const response = new ResponseOptions({ body: '<svg id="pencil"></svg>'});
+
+      backend.connections.subscribe((connection: MockConnection) => {
+        connection.mockRespond(new Response(response));
+      });
+    });
+
+    it('should project response correctly', fakeAsync(() => {
+      component.load('pencil');
+      tick();
+      expect(nativeElement.querySelector('#pencil')).not.toBeNull();
+    }));
   });
 });
