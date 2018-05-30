b177af349e436d4476e9beed19ce6cafaab090ab
Evgeny Glukhov <evglukhov@luxoft.com>
2018-05-31 00:14:38 +0300

    perfomance fixes

--- src/app/components/polyline/polyline.component.ts
+++ src/app/components/polyline/polyline.component.ts
@@ -1,16 +1,37 @@
-import { Component, Input, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  ChangeDetectionStrategy,
+  OnInit,
+  HostBinding,
+  OnChanges,
+  SimpleChanges,
+  SimpleChange
+} from '@angular/core';
 import { PolylineShape } from '@tools/shapes';
 
 @Component({
   selector: '[appPolyline]',
-  template: '',
+  template: `
+    <svg:polyline
+      [attr.points]="polyline.toString()"
+      [attr.fill]="polyline.fill"
+      [attr.stroke]="polyline.stroke"
+      [attr.stroke-width]="polyline.strokeWidth"
+    />
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PolylineComponent implements OnInit {
+export class PolylineComponent implements OnInit, OnChanges {
   @Input('appPolyline') polyline: PolylineShape;
+
   constructor() {}
 
   ngOnInit(): void {
-    console.log(this.polyline);
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
   }
 }
